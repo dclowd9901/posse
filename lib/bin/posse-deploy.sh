3bd3368bc304e1b5f44c272b4c1387bd21b53302
David Drew <david.ddrew@gmail.com>
2023-09-11 11:50:06 -0700

    rebuilds deploy script

--- lib/bin/posse-deploy.sh
+++ lib/bin/posse-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yarn ts-node ./src/scripts/build.ts
+yarn posse-build
 S3=$(cat .s3) && aws s3 cp ./build/site $S3 --recursive
 CLOUDFRONT=$(cat .cloudfront) && aws cloudfront create-invalidation --distribution-id \"$CLOUDFRONT\" --paths \"/*\"
