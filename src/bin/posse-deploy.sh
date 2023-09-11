80d5439ac3fc2b0ad895f7fb685d3edf05d35d12
David Drew <david.ddrew@gmail.com>
2023-09-11 11:59:57 -0700

    Fix source location for deploy

--- src/bin/posse-deploy.sh
+++ src/bin/posse-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yarn posse-build
-S3=$(cat .s3) && aws s3 cp ./build/site $S3 --recursive
+S3=$(cat .s3) && aws s3 cp ./public $S3 --recursive
 CLOUDFRONT=$(cat .cloudfront) && aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT" --paths "/*"
